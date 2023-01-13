8427173de80119f466375c1faf0d97a4aad25cc8
Nam JangHyun <smash009@naver.com>
2023-01-13 21:32:46 +0900

    Feat: 카운트 어플

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>count: {count}</h1>
+      <button onClick={increase}>increase</button>
     </div>
   );
 }
