6afa4079251a96d5f72f0757955d1593b9551e81
Nam JangHyun <smash009@naver.com>
2023-01-13 21:58:41 +0900

    Feat: 리덕스 적용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-// import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementCounter } from "./store/counter";
-
-// import INCREMENT = "counter/INCREMENT";
 
 function App() {
   const count = useSelector((store) => store.counter);
   const dispatch = useDispatch();
-
-  // const [count, setCount] = useState(0);
-  // const increase = () => {
-  //   setCount((prev) => prev + 1);
-  // };
 
   const increase = () => {
     const increaseAction = incrementCounter();
